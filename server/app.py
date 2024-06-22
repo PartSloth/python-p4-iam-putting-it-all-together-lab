bd26051adce708b630f0c93c10cd451c426c3d9f
Louiza Mak <louiza.mak@gmail.com>
2024-06-22 18:26:08 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,79 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+        image = data.get("image_url")
+        bio = data.get("bio")
+
+        user = User(username=username, image_url=image, bio=bio)
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            session["user_id"] = user.id
+            return user.to_dict(), 201
+        except IntegrityError:
+            return {"message": "Sign-up failed, username already exists."}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {"message": "Unauthorized access."}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+        user = User.query.filter(username == User.username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict()
+        else:
+            return {"message": "Incorrect username or password."}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {"message": "Unauthorized access."}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            recipes_dict = [recipe.to_dict() for recipe in Recipe.query.filter(session['user_id'] == Recipe.user_id).all()]
+            return recipes_dict, 200
+        else:
+            return {"message": "Unauthorized access."}, 401
+    
+    def post(self):
+        if session['user_id']:
+            recipe_data = request.get_json()
+            recipe = Recipe(
+                title = recipe_data.get("title"),
+                instructions = recipe_data.get("instructions"),
+                minutes_to_complete = recipe_data.get("minutes_to_complete"),
+                user_id = session['user_id']
+            )
+            try:
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+            except IntegrityError:
+                return {"message": "Recipe is not valid."}, 422
+        else:
+            return {"message": "You are not logged in."}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
